d922dca8863cb60df80ba563a6ca414f357cd123
QuimeyEspinosa <quimey.espinosa@hotmail.com>
2021-03-30 22:05:31 -0300

    Clase 3

---
+++ Clase 3/introduccion.js
@@ -0,0 +1,5 @@
+/*JavaScript es un lenguaje interpretado es decir que se ejecuta y se compila linea a linea*/
+
+/*Debilmente tipado tiene tipos de datos pero no hay que especificarlos cuando declaramos una variable */
+
+/* tipado dinamico al declarar la variable ésta no mantiene su tipo*/
