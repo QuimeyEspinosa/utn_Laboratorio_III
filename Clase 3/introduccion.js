4444005e8d8ecef96c663cb8452153030a9d9291
Qespinosa <qespinos@directvla.com.ar>
2022-03-22 19:00:09 -0300

    Reinicio cuatrimestre

--- Clase 3/introduccion.js
+++
@@ -1,5 +0,0 @@
-/*JavaScript es un lenguaje interpretado es decir que se ejecuta y se compila linea a linea*/
-
-/*Debilmente tipado tiene tipos de datos pero no hay que especificarlos cuando declaramos una variable */
-
-/* tipado dinamico al declarar la variable ésta no mantiene su tipo*/
